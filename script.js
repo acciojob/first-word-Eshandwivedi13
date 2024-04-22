b232f16140c85a9bbbbb427851927f3191d94330
Eshandwivedi13 <79198616+Eshandwivedi13@users.noreply.github.com>
2024-04-22 15:27:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function firstWord(s) {
-  // your code here
+	s =s.trim();
+	let arr = s.split(" ");
+   if(arr.length <= 1) return s;
+	return arr[0]; 
 }
-
+  
 // Do not change the code below
 
-const s = prompt("Enter String:");
+const s = prompt("Enter String:"); 
 alert(firstWord(s));
